79c00204e2e8d7f1e3c4edad7cf7a0d5577a2410
SJ-KrabMan <seanhig223@gmail.com>
2024-04-21 15:03:30 -0700

    this is a push w/ gameObj & wall functional

--- PA9/map.hpp
+++ PA9/map.hpp
@@ -1,12 +1,28 @@
 #include <SFML/Graphics.hpp>
 #include "gameObj.hpp"
+#include "wall.hpp"
 
 class Map : public GameObj {
 
 public:
+	//creates wall objects around perimeter of map
+	void loadMap(const sf::Vector2f& mapSize) {
+		//consider declare mapsize const in main
+		float wallThickness = 50.0f;
+		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0.0, 0.0))); //topwall 
+		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0, mapSize.y - wallThickness))); //botwall
+		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(0.0, 0.0))); //leftwall
+		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(mapSize.x - wallThickness, 0))); //rightwall
+	}
+
+	void draw(sf::RenderWindow& window) {
+		for (auto& wall : walls) {
+			wall.draw(window);
+		}
+	}
 
 private:
-
+	std::vector<Wall> walls;
 
 };
 
