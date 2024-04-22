36a1a39a42b3e17488f9d9d8c252b844e7cb896b
SJ-KrabMan <seanhig223@gmail.com>
2024-04-21 19:31:36 -0700

    big update removed all old pong comments. classes stil exist. Map works. Check collision

--- PA9/main.cpp
+++ PA9/main.cpp
@@ -6,24 +6,17 @@
 #include "wall.hpp"
 #include "map.hpp"
 
-//JP was here
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML workEvent(event)");
-	//sf::CircleShape shape(100.f);
+	
 
 	Wall wall1(sf::Vector2f(20, 200), sf::Vector2f(0, 0), sf::Color::Cyan);
 	Wall wall2(sf::Vector2f(20, 200), sf::Vector2f(500, 500), sf::Color::Red);
 
-	//Map map1(sf::Vector2f(1000, 1000));
+	Map map1(sf::Vector2f(1000, 1000));
 
-	//Paddle p1paddle(sf::Vector2f(20, 200), sf::Vector2f(0, 0), sf::Color::Green);
-		//p2paddle(sf::Vector2f(20, 200), sf::Vector2f(980, 0), sf::Color::Green);
-
-	int direction = 1, ballDirection = 1;
-
-	//shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -33,54 +26,15 @@ int main()
 			}
 		}
 
-		//gameBall.move(.4 * ballDirection, 0);
-
-		//if (p2paddle.getGlobalBounds().intersects(gameBall.getGlobalBounds())) {
-		//	ballDirection *= -1;
-		//}
-
-		////p1 paddle up/down
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		//	//not case sensitive
-		//	p1paddle.move(0, 1 * direction);
-		//}
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		//	p1paddle.move(0, 1 * -(direction));
-		//}
-		////p2 paddle up/down
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		//	p2paddle.move(0, 1 * direction);
-		//}
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		//	p2paddle.move(0, 1 * -(direction));
-		//}
-
-
-
-
-		//this happened pre pull request
-
-		// peeep this shit boiiisss
-
-
-
-		//dope ass comment
+	
 
 
 		window.clear();
-		//window.draw(shape);
-		//window.draw(gameBall);	
-		//window.draw(p1paddle);
-		//window.draw(p2paddle);
 		window.draw(wall1);
 		window.draw(wall2);
-		//wall1.draw(window);
-		/*wall2.draw(window);
-		map1.draw(window);*/
+		map1.draw(window);
 		window.display();
 
-		std::cout << "s" << std::endl; // leeevi boom
-		std::cout << "3" << std::endl;
 	}
 
 	return 0;
--- PA9/map.hpp
+++ PA9/map.hpp
@@ -2,42 +2,42 @@
 #include "gameObj.hpp"
 #include "wall.hpp"
 
-//class Map : public GameObj {
-//
-//public:
-//	//creates wall objects around perimeter of map
-//	Map(const sf::Vector2f& mapSize) {
-//		loadMap(mapSize);
-//	}
-//
-//	void loadMap(const sf::Vector2f& mapSize) {
-//		//consider declare mapsize const in main
-//		float wallThickness = 0.01f;
-//		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0.0, 0.0))); //topwall 
-//		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0, mapSize.y - wallThickness))); //botwall
-//		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(0.0, 0.0))); //leftwall
-//		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(mapSize.x - wallThickness, 0))); //rightwall
-//
-//		//obstacles
-//		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(10, 10)));
-//		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(250, 100)));
-//		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(300, 150)));
-//		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(900, 500)));
-//
-//	}
-//
-//	void draw(sf::RenderWindow& window) {
-//		for (auto& wall : walls) {
-//			wall.draw(window);
-//		}
-//	}
-//
-//	virtual void update(const sf::Time& dt) override {
-//		//wall wont update. 90% sure I still needed to define this func tho to prevent compile issues.
-//	}
-//
-//private:
-//	std::vector<Wall> walls;
-//
-//};
+class Map : public GameObj {
+
+public:
+	//creates wall objects around perimeter of map
+	Map(const sf::Vector2f& mapSize) {
+		loadMap(mapSize);
+	}
+
+	void loadMap(const sf::Vector2f& mapSize) {
+		//consider declare mapsize const in main
+		float wallThickness = 0.01f;
+		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0.0, 0.0), sf::Color::Transparent)); //topwall 
+		walls.push_back(Wall(sf::Vector2f(mapSize.x, wallThickness), sf::Vector2f(0, mapSize.y - wallThickness), sf::Color::Transparent)); //botwall
+		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(0.0, 0.0), sf::Color::Transparent)); //leftwall
+		walls.push_back(Wall(sf::Vector2f(wallThickness, mapSize.y), sf::Vector2f(mapSize.x - wallThickness, 0), sf::Color::Transparent)); //rightwall
+
+		//obstacles
+		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(10, 10), sf::Color::Green));
+		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(250, 100), sf::Color::Green));
+		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(300, 150), sf::Color::Green));
+		walls.push_back(Wall(sf::Vector2f(20, 20), sf::Vector2f(900, 500), sf::Color::Green));
+
+	}
+
+	void draw(sf::RenderWindow& window) {
+		for (auto& wall : walls) {
+			window.draw(wall);
+		}
+	}
+
+	virtual void update(const sf::Time& dt) override {
+		//wall wont update. 90% sure I still needed to define this func tho to prevent compile issues.
+	}
+
+private:
+	std::vector<Wall> walls;
+
+};
 
