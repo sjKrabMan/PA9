a53b0faa81fb69a955cd309bed049fcc42b55e1a
leviCP <levichapman0@gmail.com>
2024-04-22 08:28:54 -0700

    this new addiotion is all the movement and collision for tank_p1

--- PA9/main.cpp
+++ PA9/main.cpp
@@ -5,6 +5,7 @@
 //#include "gameObj.hpp"
 #include "wall.hpp"
 #include "map.hpp"
+#include "tank.hpp"
 
 //JP was here
 
@@ -16,6 +17,10 @@ int main()
 	Wall wall1(sf::Vector2f(20, 200), sf::Vector2f(0, 0), sf::Color::Cyan);
 	Wall wall2(sf::Vector2f(20, 200), sf::Vector2f(500, 500), sf::Color::Red);
 
+	Tank p1tank(sf::Vector2f(200, 100), sf::Color::Green);
+
+	//Tank p2tank(sf::Vector2f(40, 40), sf::Vector2f(840, 840), sf::Color::Yellow);
+
 	//Map map1(sf::Vector2f(1000, 1000));
 
 	//Paddle p1paddle(sf::Vector2f(20, 200), sf::Vector2f(0, 0), sf::Color::Green);
@@ -25,48 +30,127 @@ int main()
 
 	//shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
-		//gameBall.move(.4 * ballDirection, 0);
+        //gameBall.move(.4 * ballDirection, 0);
 
-		//if (p2paddle.getGlobalBounds().intersects(gameBall.getGlobalBounds())) {
-		//	ballDirection *= -1;
-		//}
+        //if (p2paddle.getGlobalBounds().intersects(gameBall.getGlobalBounds())) {
+        //	ballDirection *= -1;
+        //}
 
-		////p1 paddle up/down
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		//	//not case sensitive
-		//	p1paddle.move(0, 1 * direction);
-		//}
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		//	p1paddle.move(0, 1 * -(direction));
-		//}
-		////p2 paddle up/down
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		//	p2paddle.move(0, 1 * direction);
-		//}
-		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		//	p2paddle.move(0, 1 * -(direction));
-		//}
-
-
-
-
-		//this happened pre pull request
-
-		// peeep this shit boiiisss
+        ////p1 paddle up/down
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        //	//not case sensitive
+        //	p1paddle.move(0, 1 * direction);
+        //}
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        //	p1paddle.move(0, 1 * -(direction));
+        //}
+        ////p2 paddle up/down
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+        //	p2paddle.move(0, 1 * direction);
+        //}
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+        //	p2paddle.move(0, 1 * -(direction));
+        //}
 
 
 
-		//dope ass comment
+        ///////////////this is all the movement used for tank player 1//////////////////////////////////////////
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            p1tank.move(0, -0.05);
+            p1tank.setRotation(270.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            p1tank.move(0, 0.05);
+            p1tank.setRotation(90.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            p1tank.move(0.05, 0);
+            p1tank.setRotation(0.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            p1tank.move(-0.05, 0);
+            p1tank.setRotation(180.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            p1tank.setRotation(315.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            p1tank.setRotation(225.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            p1tank.setRotation(135.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            p1tank.setRotation(45.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            p1tank.setRotation(90.f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            p1tank.setRotation(270.f);
+        }
+        ///////////////this it the collision detection for tank player 1//////////////////////////////////////
+        if (p1tank.getGlobalBounds().intersects(wall1.getGlobalBounds()))
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                p1tank.move(0, 0.05);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                p1tank.move(0, -0.05);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            {
+                p1tank.move(-0.05, 0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                p1tank.move(0.05, 0);
+            }
+        }
 
-		// Ethan is here
+        if (p1tank.getGlobalBounds().intersects(wall2.getGlobalBounds()))
+        {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                p1tank.move(0, 0.05);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                p1tank.move(0, -0.05);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            {
+                p1tank.move(-0.05, 0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                p1tank.move(0.05, 0);
+            }
+        }
+    
+
+
 
 
 
@@ -80,10 +164,15 @@ int main()
 		//wall1.draw(window);
 		/*wall2.draw(window);
 		map1.draw(window);*/
+
+		window.draw(p1tank);
+		//window.draw(p2tank);
+
 		window.display();
 
-		std::cout << "s" << std::endl; // leeevi boom
-		std::cout << "3" << std::endl;
+
+		//std::cout << "s" << std::endl; // leeevi boom
+		//std::cout << "3" << std::endl;
 	}
 
 	return 0;
